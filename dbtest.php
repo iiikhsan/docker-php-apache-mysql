610ac0b0e154084fa1c48341356ede0dd3f5b3cb
iiikhsan <demonspeedster@gmail.com>
2019-07-07 21:25:11 +0700

    Create dbtest.php

---
+++ dbtest.php
@@ -0,0 +1,13 @@
+<h1>Test Page!</h1>
+<h4>Attempting MySQL connection from php...</h4>
+<?php
+$host = 'db';
+$user = 'root';
+$pass = '123456';
+$conn = new mysqli($host, $user, $pass);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+echo "Connected to MySQL successfully!";
+?>
